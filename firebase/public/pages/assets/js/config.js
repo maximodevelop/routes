f858aced71e19429883e0224744f5d6b8d8d6247
suzdalenko.suzdalenko@gmail.com <suzdalenko.suzdalenko@gmail.com>
2023-06-01 12:35:10 +0200

    j

---
+++ firebase/public/pages/assets/js/config.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyDZ6cpfLEHB6YTN_C1UXw5dpaOuO3041s8",
+    authDomain: "x-route-planning.firebaseapp.com",
+    databaseURL: "https://x-route-planning-default-rtdb.firebaseio.com",
+    projectId: "x-route-planning",
+    storageBucket: "x-route-planning.appspot.com",
+    messagingSenderId: "536796594344",
+    appId: "1:536796594344:web:bfff3ae5a787d1a457029d",
+    measurementId: "G-VHTNMZ1N44"
+}
